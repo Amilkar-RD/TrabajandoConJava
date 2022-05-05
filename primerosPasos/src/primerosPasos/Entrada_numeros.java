504cae81d431dfbcf02f9a4e3a19899aed321398
Amilkar-RD <blacksmayther@hotmail.com>
2022-05-05 01:31:40 -0500

    Entrada de datos numericos

---
+++ primerosPasos/src/primerosPasos/Entrada_numeros.java
@@ -0,0 +1,18 @@
+package primerosPasos;
+import javax.swing.*;
+
+public class Entrada_numeros {
+	
+	public static void main(String [] args) {
+		
+		String num1 = JOptionPane.showInputDialog("Introduce un número");
+		
+		double num2 = Double.parseDouble(num1);
+		
+		
+		System.out.print("La raíz de " + num2 + " es ");
+		
+		System.out.print(Math.sqrt(num2));		
+	}
+
+}
