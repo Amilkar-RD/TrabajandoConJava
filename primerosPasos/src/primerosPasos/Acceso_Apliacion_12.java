45a7e5b361091fa2b33d5a31f1e4f09a06c887d3
Amilkar-RD <blacksmayther@hotmail.com>
2022-05-05 02:47:24 -0500

    Trabajando con flujo de control - while

---
+++ primerosPasos/src/primerosPasos/Acceso_Apliacion_12.java
@@ -0,0 +1,23 @@
+package primerosPasos;
+import javax.swing.*;
+
+public class Acceso_Apliacion_12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String clave ="amilkar";
+		
+		String pass = "";
+		
+		while(clave.equals(pass) ==false) {
+			pass = JOptionPane.showInputDialog("Introduce la contraseña por favor");
+			if(clave.equals(pass)==false) {
+				System.out.println("COntraseña incorecta");
+			}
+		}
+		System.out.println("Contraseña correcta. Acceso permitido");
+		
+	}
+
+}
